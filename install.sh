c56ce9471d1f200afb601a5fa6ce8857337eeb20
Jan Forman <48495095+janforman@users.noreply.github.com>
2021-05-04 20:24:54 +0200

    ceph mon hostname

--- install.sh
+++ install.sh
@@ -247,7 +247,7 @@ elif [ $input == "ceph" ]; then
             FSID="$(uuidgen)"
             echo "[global]" | sudo tee /etc/ceph/ceph.conf
             echo "fsid = $FSID" | sudo tee -a /etc/ceph/ceph.conf
-            echo "mon initial members = $IP" | sudo tee -a /etc/ceph/ceph.conf
+            echo "mon initial members = $HOSTNAME" | sudo tee -a /etc/ceph/ceph.conf
             echo "mon host = $IP" | sudo tee -a /etc/ceph/ceph.conf
             printf "auth cluster required = cephx\nauth service required = cephx\nauth client required = cephx\nosd journal size = 1024\nosd pool default size = 3\nosd pool default min size = 2\nosd pool default pg num = 333\nosd pool default pgp num = 333\nosd crush chooseleaf type = 1\n" | sudo tee -a /etc/ceph/ceph.conf
 	    sudo mkdir /var/lib/ceph/bootstrap-osd/
